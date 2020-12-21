cbd15d7f5383c8071f3352545169e231465d0883
Alex W <main@Alexs-MacBook-Pro-2.local>
2020-12-21 16:07:06 +1100

    updates

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 
-const etherscanApiKey = null;
-const web3ProviderURL = null;
+const etherscanApiKey = "T69KANUMP7JM9VPKUZUXTJCTYTGRTWFAKA";
+const web3ProviderURL = "http://127.0.0.1:8545"
 
 
 module.exports ={
